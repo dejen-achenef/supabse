9af0a687bd6ac73f0eee364be69c2dec8b1f4155
dejen-achenef <dejen.achenef-ug@aau.edu.et>
2025-12-17 23:32:49 +0300

    adding this file

---
+++ lib/pages/leau.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class lovess extends StatelessWidget {
+  const lovess({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
