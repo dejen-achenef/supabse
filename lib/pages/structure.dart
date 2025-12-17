cbe8d0f46e9d4d3f58c1be0177e96ce05889119a
dejen-achenef <dejen.achenef-ug@aau.edu.et>
2025-12-17 23:33:44 +0300

    baldnet file

---
+++ lib/pages/structure.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Baldnet extends StatefulWidget {
+  const Baldnet({super.key});
+
+  @override
+  State<Baldnet> createState() => _BaldnetState();
+}
+
+class _BaldnetState extends State<Baldnet> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
