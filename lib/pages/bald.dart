d35e476f6cccecb8744f4ee4bcc91bdc3dd0ac96
dejen-achenef <dejen.achenef-ug@aau.edu.et>
2025-12-17 23:32:04 +0300

    adding detial

---
+++ lib/pages/bald.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Bald extends StatefulWidget {
+  const Bald({super.key});
+
+  @override
+  State<Bald> createState() => _BaldState();
+}
+
+class _BaldState extends State<Bald> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
